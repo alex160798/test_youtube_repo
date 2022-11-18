384192eebff15a1d3dfd36a2c5c7bc582b371e48
Aleksei <alex160798@gmail.com>
2022-11-18 18:16:15 +0300

    Added pytest (first half)

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
 print('Hello world!')
+
+import pytest
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::-1]
+
+@pytest.mark.parametrize("input_value, expected", [
+    (123, False),
+    (111, True),
+    (2222, True),
+])
+
+def test_palindrom(input_value, expected):
+    pass
