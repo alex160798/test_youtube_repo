c2dc652bb74f28e643b5653e293bad19ba13cb68
Aleksei <alex160798@gmail.com>
2022-11-20 16:28:35 +0300

    Testing pytest

--- pytesting/tests/test_division_func.py
+++ pytesting/tests/test_division_func.py
@@ -1,6 +1,16 @@
 from utils import division
+import pytest
 
 
-def test_division_good():
-    assert division(10, 2) == 5
+@pytest.mark.parametrize('a, b, result', [(2, 1, 2),
+                                          (10, 5, 2),
+                                          (3, -1, -3), ])
+def test_division_good(a, b, result):
+    assert division(a, b) == result
 
+
+@pytest.mark.parametrize('divider, exception', [(0, ZeroDivisionError),
+                                                ('2', TypeError), ])
+def test_exceptions(divider, exception):
+    with pytest.raises(exception):
+        division(10, divider)
